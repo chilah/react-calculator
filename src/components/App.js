9e4cb78484ba7f7a1f3a2a8c18ede76941fc07e8
chilah <pasitaless@gmail.com>
2019-04-20 00:56:57 +0700

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,114 @@
-import React from 'react';
+import React, { Component } from "react";
+import Keypad from "./Keypad";
+import "./style/App.css";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayValue: "",
+      prevValue: "",
+      isOperator: false,
+      error: false
+    };
+  }
 
-function App() {
-  return (
-    <div>
-      <h1 className="text-primary">Hello from App component!</h1>
-    </div>
-  )
+  inputValue = e => {
+    const { name } = e.target;
+
+    if (this.state.isOperator) {
+      this.setState(state => {
+        return {
+          displayValue: name,
+          prevValue: state.prevValue + name,
+          isOperator: false
+        };
+      });
+    } else {
+      this.setState(state => {
+        return {
+          displayValue: state.displayValue + name,
+          prevValue: state.prevValue + name
+        };
+      });
+    }
+  };
+
+  inputOperator = e => {
+    const { name } = e.target;
+
+    if (this.state.displayValue !== "" && !this.state.isOperator) {
+      this.setState(state => {
+        return {
+          displayValue: name,
+          prevValue: state.prevValue + name,
+          isOperator: true
+        };
+      });
+    }
+
+    if (this.state.isOperator) {
+      this.setState(state => {
+        return {
+          displayValue: name,
+          prevValue: state.prevValue.slice(0, -1) + name,
+        }
+      })
+    }
+  };
+
+  calculate = () => {
+    const calc = eval(this.state.prevValue);
+
+    this.setState(state => {
+      return {
+        displayValue: String(calc),
+        prevValue: String(calc)
+      };
+    });
+  };
+
+  deleteValue = () => {
+    const { displayValue } = this.state;
+    const num = displayValue.length > 1 ? displayValue.slice(0, -1) : "";
+
+    this.setState(state => {
+      return {
+        displayValue: num
+      };
+    });
+  };
+
+  clearValue = () => {
+    this.setState(state => {
+      return {
+        displayValue: "",
+        prevValue: "",
+        isOperator: false
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="display">
+          {this.state.error && <h3>Please fill the completely digit</h3>}
+          <h5 className="display--prevValue">
+            {this.state.prevValue ? this.state.prevValue : "0"}
+          </h5>
+          <h1>{this.state.displayValue ? this.state.displayValue : "0"}</h1>
+        </div>
+        <Keypad
+          inputValue={this.inputValue}
+          inputOperator={this.inputOperator}
+          calculate={this.calculate}
+          deleteValue={this.deleteValue}
+          clearValue={this.clearValue}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Keypad.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import "./style/Keypad.css";
+
+export class Keypad extends Component {
+  render() {
+    return (
+      <div className="keypad">
+        <button
+          name="clear"
+          onClick={this.props.clearValue}
+          className="btn btn-danger btn-lg clear__btn"
+        >
+          C
+        </button>
+        <button
+          name="delete"
+          onClick={this.props.deleteValue}
+          className="btn btn-danger btn-lg delete__btn"
+        >
+          DEL
+        </button>
+        <button
+          name="7"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          7
+        </button>
+        <button
+          name="8"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          8
+        </button>
+        <button
+          name="9"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          9
+        </button>
+        <button
+          name="/"
+          onClick={this.props.inputOperator}
+          className="btn btn-primary btn-lg"
+        >
+          /
+        </button>
+        <button
+          name="4"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          4
+        </button>
+        <button
+          name="5"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          5
+        </button>
+        <button
+          name="6"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          6
+        </button>
+        <button
+          name="*"
+          onClick={this.props.inputOperator}
+          className="btn btn-primary btn-lg"
+        >
+          *
+        </button>
+        <button
+          name="1"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          1
+        </button>
+        <button
+          name="2"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          2
+        </button>
+
+        <button
+          name="3"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          3
+        </button>
+        <button
+          name="+"
+          onClick={this.props.inputOperator}
+          className="btn btn-primary btn-lg"
+        >
+          +
+        </button>
+        <button name="." className="btn btn-dark btn-lg">
+          .
+        </button>
+        <button
+          name="0"
+          onClick={this.props.inputValue}
+          className="btn btn-dark btn-lg"
+        >
+          0
+        </button>
+        <button
+          name="="
+          onClick={this.props.calculate}
+          className="btn btn-warning btn-lg"
+        >
+          =
+        </button>
+        <button
+          name="-"
+          onClick={this.props.inputOperator}
+          className="btn btn-primary btn-lg"
+        >
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Keypad;
